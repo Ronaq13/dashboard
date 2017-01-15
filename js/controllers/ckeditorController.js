04c1d796bb8144198a8da89c890afbca77a6f910
Ronaq13 <raounaqsharma@gmail.com>
2017-01-15 22:36:29 +0530

    CSS modified

--- js/controllers/ckeditorController.js
+++ js/controllers/ckeditorController.js
@@ -35,10 +35,127 @@
                             "children": [{
                                 "roleName": "subUser2-1",
                                 "roleId": "role121",
-                                "children": [
-                                    { "roleName": "subUser2-1-1", "roleId": "role1211", "children": [] },
-                                    { "roleName": "subUser2-1-2", "roleId": "role1212", "children": [] }
-                                ]
+                                "children": []
+                            }]
+                        }
+                    ]
+                },
+
+                { "roleName": "Admin", "roleId": "role2", "children": [] },
+
+                { "roleName": "Guest", "roleId": "role3", "children": [] },
+                // extra added to check scroll
+                {
+                    "roleName": "User",
+                    "roleId": "role1",
+                    "children": [
+                        { "roleName": "subUser1", "roleId": "role11", "children": [] },
+                        {
+                            "roleName": "subUser2",
+                            "roleId": "role12",
+                            "children": [{
+                                "roleName": "subUser2-1",
+                                "roleId": "role121",
+                                "children": []
+                            }]
+                        }
+                    ]
+                },
+
+                { "roleName": "Admin", "roleId": "role2", "children": [] },
+
+                { "roleName": "Guest", "roleId": "role3", "children": [] }, {
+                    "roleName": "User",
+                    "roleId": "role1",
+                    "children": [
+                        { "roleName": "subUser1", "roleId": "role11", "children": [] },
+                        {
+                            "roleName": "subUser2",
+                            "roleId": "role12",
+                            "children": [{
+                                "roleName": "subUser2-1",
+                                "roleId": "role121",
+                                "children": []
+                            }]
+                        }
+                    ]
+                },
+
+                { "roleName": "Admin", "roleId": "role2", "children": [] },
+
+                { "roleName": "Guest", "roleId": "role3", "children": [] },
+                {
+                    "roleName": "User",
+                    "roleId": "role1",
+                    "children": [
+                        { "roleName": "subUser1", "roleId": "role11", "children": [] },
+                        {
+                            "roleName": "subUser2",
+                            "roleId": "role12",
+                            "children": [{
+                                "roleName": "subUser2-1",
+                                "roleId": "role121",
+                                "children": []
+                            }]
+                        }
+                    ]
+                },
+
+                { "roleName": "Admin", "roleId": "role2", "children": [] },
+
+                { "roleName": "Guest", "roleId": "role3", "children": [] },
+                {
+                    "roleName": "User",
+                    "roleId": "role1",
+                    "children": [
+                        { "roleName": "subUser1", "roleId": "role11", "children": [] },
+                        {
+                            "roleName": "subUser2",
+                            "roleId": "role12",
+                            "children": [{
+                                "roleName": "subUser2-1",
+                                "roleId": "role121",
+                                "children": []
+                            }]
+                        }
+                    ]
+                },
+
+                { "roleName": "Admin", "roleId": "role2", "children": [] },
+
+                { "roleName": "Guest", "roleId": "role3", "children": [] },
+                {
+                    "roleName": "User",
+                    "roleId": "role1",
+                    "children": [
+                        { "roleName": "subUser1", "roleId": "role11", "children": [] },
+                        {
+                            "roleName": "subUser2",
+                            "roleId": "role12",
+                            "children": [{
+                                "roleName": "subUser2-1",
+                                "roleId": "role121",
+                                "children": []
+                            }]
+                        }
+                    ]
+                },
+
+                { "roleName": "Admin", "roleId": "role2", "children": [] },
+
+                { "roleName": "Guest", "roleId": "role3", "children": [] },
+                {
+                    "roleName": "User",
+                    "roleId": "role1",
+                    "children": [
+                        { "roleName": "subUser1", "roleId": "role11", "children": [] },
+                        {
+                            "roleName": "subUser2",
+                            "roleId": "role12",
+                            "children": [{
+                                "roleName": "subUser2-1",
+                                "roleId": "role121",
+                                "children": []
                             }]
                         }
                     ]
@@ -47,6 +164,7 @@
                 { "roleName": "Admin", "roleId": "role2", "children": [] },
 
                 { "roleName": "Guest", "roleId": "role3", "children": [] }
+
             ];
 
             $scope.$watch('mytree.currentNode', function(newObj, oldObj) {
